060895864e04e4f18d25dce6839b7fd233433695
kkosnik <kelly.kosnik@kellogg.northwestern.edu>
2022-02-22 16:20:02 +0000

    save partway through

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        entered_email = params["email"]
+        entered_password = params["password"]
+        @user = User.find_by({email: entered_email})
+
+        if @user
+            if BCrypt::Password.new(@user.password) == entered_password
+                session["user_id"] = @user.id
+                flash[:notice] = "Welcome!"
+                redirect_to "/companies"
+            else
+                flash[:notice] = "Password is incorrect"
+                redirect_to "/sessions/new"
+            end
+        else
+            flash[:notice] = "No user with that email address"
+            redirect_to "/sessions/new"
+        end
+    end
+
+end
