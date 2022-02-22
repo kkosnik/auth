49905824680f33221230961a023db12d616188d0
kkosnik <kelly.kosnik@kellogg.northwestern.edu>
2022-02-22 17:30:27 +0000

    finished class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,4 +23,10 @@ class SessionsController < ApplicationController
         end
     end
 
+    def destroy
+        session["user_id"] = nil
+        flash[:notice] = "You have been logged out"
+        redirect_to "/sessions/new"
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users
   resources :sessions
+  root to: "companies#index"
 end
